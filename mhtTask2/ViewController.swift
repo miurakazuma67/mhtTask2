380f183c340762149613daf76b7034ad67ee5c06
miurakazuma67 <miurakazuma67@gmail.com>
2021-03-09 09:10:58 +0900

    Initial Commit

---
+++ mhtTask2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mhtTask2
+//
+//  Created by 三浦　一真 on 2021/03/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
